e96e46a0f0f419a1d4f9e2659614ade1b82b846d
Eduardo Sa <nofrereis@gmail.com>
2021-05-26 23:51:45 -0300

    primeiro commit

---
+++ src/main/java/com/api/java/spring/rabbit/mq/app/estudo/JavaSpringRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.api.java.spring.rabbit.mq.app.estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRabbitMqApplication.class, args);
+	}
+
+}
