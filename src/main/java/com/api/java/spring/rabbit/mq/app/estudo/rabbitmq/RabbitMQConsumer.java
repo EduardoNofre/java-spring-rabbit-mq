96c9793df09ab6d9099171b34b3fd2dc77a493d9
Eduardo Sa <nofrereis@gmail.com>
2021-05-27 00:40:27 -0300

    readme melhorias

---
+++ src/main/java/com/api/java/spring/rabbit/mq/app/estudo/rabbitmq/RabbitMQConsumer.java
@@ -0,0 +1,13 @@
+package com.api.java.spring.rabbit.mq.app.estudo.rabbitmq;
+
+
+/**
+ * 
+ * @author nofre
+ *
+ *Consuming – O recepetor da mensagem
+ *
+ */
+public class RabbitMQConsumer {
+
+}
---
+++ src/main/java/com/api/java/spring/rabbit/mq/app/estudo/rabbitmq/RabbitMQProducing.java
@@ -0,0 +1,13 @@
+package com.api.java.spring.rabbit.mq.app.estudo.rabbitmq;
+
+
+/**
+ * 
+ * @author nofre
+ * 
+ *Producing – O produtor da mensagem
+ *
+ */
+public class RabbitMQProducing  {
+
+}
